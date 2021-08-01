fe16395fc7657db61aac80a38da57b9fe2463147
Niwa Miki <s.miki694@gmail.com>
2021-08-01 14:17:55 +0900

    ポートフォリオ修正

--- public/js/script.js
+++ public/js/script.js
@@ -1,8 +1,6 @@
 $(window).on('load',function(){
-  // TOP画像表示アニメーション
   $('#top_banner').hide().fadeIn(2000);
-  
-  // 上から100pxスクロールした場合、TOPスクロールボタンを表示
+
   var scrollTop = $('.fa-chevron-circle-up');
   $(window).scroll(function () {
       if ($(this).scrollTop() > 110) {
@@ -11,7 +9,6 @@ $(window).on('load',function(){
           scrollTop.fadeOut();
       }
   });
-  // スクロールボタン押下時アニメーション
   scrollTop.click(function () {
       $('body, html').animate({scrollTop: 0}, 500);
       return false;
